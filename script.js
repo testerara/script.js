48b523450227d1b18590735513e9c3cd13f93628
testerara <ozatyd8@gmail.com>
2026-05-25 00:39:51 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-console.log("JS is working");
-document.body.innerHTML += "Hello from JS";
-console.log("JS is working");
+console.log("JS is working 🚀");
+
+// تأكيد التنفيذ في الـ Console
+console.log("Script executed successfully");
+
+// تعديل الـ DOM بشكل واضح
+document.body.innerHTML += "<div style='color:red;font-size:20px;'>Hello from JS</div>";
+
+// إضافة عنصر جديد بدل innerHTML (أفضل في الاختبار)
+var el = document.createElement("div");
+el.innerText = "Element created by JS";
+el.style.color = "blue";
+document.body.appendChild(el);
+
+// علامة واضحة جدًا في الصفحة
+document.body.style.border = "5px solid green";
